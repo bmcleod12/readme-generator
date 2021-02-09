e8742a82fae1f5681bc274e77098baae11f76b32
Brenna McLeod <brenna.mcleod94@gmail.com>
2021-02-09 09:59:52 -0600

    question array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,49 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:  'input',
+        name: 'description',
+        message: 'Provide a Description of your application.',
+    },
+    {
+        type:  'input',
+        name: 'installation',
+        message: 'Provide installation instructions for your application.',
+    },
+    {
+        type:  'input',
+        name: 'usage',
+        message: 'Provide instructions for the usage of your application.',
+    },
+    {
+        type:  'input',
+        name: 'contribution',
+        message: 'Provide instructions on how to contribue to your application, if appropriate.',
+    },
+    {
+        type:  'input',
+        name: 'appTest',
+        message: 'Provide a few test cases users can run on your application.',
+    },
+    {
+        type:  'list',
+        name: 'license',
+        message: 'Select a license to apply to your application.',
+        choices: ['']
+    },
+    {
+        type:  'input',
+        name: 'gitHubUsername',
+        message: 'Please enter your GitHub username.',
+    },
+    {
+        type:  'input',
+        name: 'emailAddress',
+        message: 'Please enter your email address to be used as a point of contact for questions.',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
